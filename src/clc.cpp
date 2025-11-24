cf57fa50642afbc7e712cb66df8f7fe2afedc912
gfaerny <gfaerny@gmail.com>
2025-11-25 00:02:35 +0330

    writing string converet function for time - output_time = 62.1000 -> clc show time -> 1.2.1 -					    m s s/10

--- src/clc.cpp
+++ src/clc.cpp
@@ -1,4 +1,5 @@
 #include <X11/X.h>
+#include <X11/Xfuncproto.h>
 #include <cstdint>
 #include <cstdlib>
 #include <ctime>
@@ -14,25 +15,48 @@
 #define RGFW_IMPLEMENTATION
 #define RGFW_OPENGL
 
-#define GLYPHGL_MINIMAL
-
 #include "../include/RGFW/RGFW.h"
 #include "../include/glyph/glyph.h"
 
 #include <GL/gl.h>
 
-
 using namespace std;
 using namespace chrono;
 namespace fs = std::filesystem;
 
-auto start = high_resolution_clock::now();
-
 double savedTime = 0 , output_time = 0;
-
-bool running = false;
 fs::path homeDir = getenv("HOME");
 fs::path filePath = homeDir / ".clc" / "lt";
+
+
+static  std::string t_str_fucn (double &time)
+{
+    std::string function_result = "";    
+
+    if(time >= 3600)
+    {
+        int hour = time / 3600;
+        function_result += std::to_string(hour);
+        function_result += ".";
+
+        int min = std::fmod(time , 3600) / 60;
+
+        function_result +=std::to_string(min);
+        function_result += ".";        
+    }
+    else if (time >= 60)
+    {
+        int min = std::fmod(time,3600) / 60;
+        function_result += std::to_string(min);
+        function_result += ".";   
+    }
+    
+        double sec = std::fmod(time , 60);
+        function_result +=std::to_string(sec);
+
+        function_result.erase(6);    
+    return function_result;
+}
 
 
 void saveTime(long long total_ms)
@@ -71,31 +95,20 @@ double loadTime()
     return previousTime;
 }
 
-void on_exit() 
-{
-    auto now = high_resolution_clock::now();
-    if (running) 
-    {
-        savedTime += duration_cast<milliseconds>(now - start).count();
-    }
-    std::cerr << savedTime << "\n";
-    saveTime(savedTime);
-}
 
 int main(void)
 {
-///  atexit(on_exit);
 
     double additional_time , last_time = 0;
-/// if order_time_stop == true mean's timer has stop counting
+
     bool order_time_stop = true;
-/*
+
     if(!(loadTime() == 0))
     {
         savedTime = loadTime();
     }
-*/
 
+        
     uint8_t min = 0 , hour = 0 , sec = 0;  
     
     RGFW_glHints* hints = RGFW_getGlobalHints_OpenGL();
@@ -103,18 +116,15 @@ int main(void)
     hints->minor = 3;
     RGFW_setGlobalHints_OpenGL(hints);
 
-   /// RGFW_window* RGFW_window_obj = RGFW_createWindow("clc.", 0, 0, 500, 300, RGFW_windowNoBorder || RGFW_windowNoResize || RGFW_windowOpenGL);
-    RGFW_window* RGFW_window_obj = RGFW_createWindow("clc.", 0, 0, 500, 300, RGFW_windowCenter || RGFW_windowNoResize ||/*RGFW_windowNoBorder ||*/ RGFW_windowHide);
+   
+    RGFW_window* RGFW_window_obj = RGFW_createWindow("clc.", 0, 0, 500, 300, RGFW_windowCenter || RGFW_windowNoResize || RGFW_windowNoBorder || RGFW_windowOpenGL);
     RGFW_window_createContext_OpenGL(RGFW_window_obj, hints);
 
     RGFW_window_show(RGFW_window_obj);
 
-    const GLubyte *version = glGetString(GL_VERSION);
-
     glyph_renderer_t renderer = glyph_renderer_create("font.ttf", 135.0f,NULL, GLYPH_UTF8, NULL, 0);
     glyph_renderer_set_projection(&renderer, 800, 600);
-    ///glyph_renderer_create(const char *font_path, float pixel_height, const char *charset, uint32_t char_type, void *effect, int use_sdf)
-
+    
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
@@ -150,47 +160,28 @@ int main(void)
     /// graphic interface    
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
         if(order_time_stop)
         {
             savedTime = output_time;
-            /*
-            sec = std::fmod(output_time , 60);
-            min = (output_time / 60);
-            hour = output_time / 3600;
-            /*
-            std::cout <<"fmod :" << test<<'\n'  << "output_time solied : " << output_time << '\n';
-*//*
-            if(hour >= 1)
-            {
-                t_str = std::to_string(hour);
-                t_str += "." + std::to_string(min);
-                t_str += "." + std::to_string(sec);
-            }
 
-            else if(min >= 1)
-            {
-                t_str = std::to_string(min);
-                t_str += "." +std::to_string(sec);
-            }
-          */
-            t_str = std::to_string(output_time);
-            t_str.erase(5);
-            
+            t_str = t_str_fucn(output_time);
+ 
+            std::cout << output_time << '\n';
             glyph_renderer_draw_text(&renderer, t_str.c_str(),230.0f, 350.0f, 1.0f, 1.0f, 1.0f, 1.0f, GLYPH_NONE);
             
-///         
             last_time_before_stop = high_resolution_clock::now();
         }
+
         else
         {    
             auto now_time = high_resolution_clock::now();
             output_time = duration<double>(now_time - start_time).count();
-            std::cout << output_time + savedTime << '\n' << output_time << savedTime;
             output_time+=savedTime;
             
-            t_str = std::to_string(output_time);
-            t_str.erase(5);
-        
+            std::cout << output_time << '\n';
+            t_str = t_str_fucn(output_time);
+       
             glyph_renderer_draw_text(&renderer, t_str.c_str(),230.0f, 350.0f, 1.0f, 1.0f, 1.0f, 1.0f, GLYPH_NONE);
         }
         glyph_renderer_draw_text(&renderer,"clc.", 10, 100, 1.0f, 1.0f, 1.0f, 1.0f, GLYPH_NONE);
