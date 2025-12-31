62c01d14755545e59b8e080fa340f346af5f995b
gfaerny <gfaerny@gmail.com>
2026-01-01 00:38:37 +0330

    trying to fix building problem.  i change address file for glyph and RGFW

--- install_dependency.sh
+++ install_dependency.sh
@@ -1,4 +1,4 @@
 # install RGFW - Glyph lib for build
-git clone https://github.com/ColleagueRiley/RGFW.git include/RGFW/
-git clone https://github.com/DareksCoffee/GlyphGL.git include/glyph
+git clone https://github.com/ColleagueRiley/RGFW.git /RGFW
+git clone https://github.com/DareksCoffee/GlyphGL.git /glyph
 
