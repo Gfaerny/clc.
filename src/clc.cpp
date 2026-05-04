8acc9f5a9309b629485856746f58a25283f22d54
Iliya Abolghasemi <gfaerny@gmail.com>
2026-05-04 14:03:26 +0330

    New CI name build test Format cpp file

--- src/clc.cpp
+++ src/clc.cpp
@@ -2,42 +2,41 @@
 
 namespace fs = std::filesystem;
 
-double saved_time {0} , output_time {0} , rus_time {0};
+double saved_time{0}, output_time{0}, rus_time{0};
 const fs::path home_dir = getenv("HOME");
 const fs::path saved_time_file_path = home_dir / ".clc" / "lt";
-const char font_path[] {"/usr/share/clc/font.ttf"};
+const char font_path[]{"/usr/share/clc/font.ttf"};
 
 std::string t_str = "";
 
-std::string t_str_fucn (double &time)
+std::string t_str_fucn(double &time)
 {
-    std::string function_result = "";    
-    
-    if(time >= 3600)
+    std::string function_result = "";
+
+    if (time >= 3600)
     {
         int hour = time / 3600;
         function_result += std::to_string(hour);
         function_result += ".";
 
-        int min = std::fmod(time , 3600) / 60;
+        int min = std::fmod(time, 3600) / 60;
 
-        function_result +=std::to_string(min);
-        function_result += ".";        
+        function_result += std::to_string(min);
+        function_result += ".";
     }
     else if (time >= 60)
     {
-        int min = std::fmod(time,3600) / 60;
+        int min = std::fmod(time, 3600) / 60;
         function_result += std::to_string(min);
-        function_result += ".";   
+        function_result += ".";
     }
-    
-        double sec = std::fmod(time , 60);        
-        function_result +=std::to_string(sec);
 
-        function_result.erase(8);    
+    double sec = std::fmod(time, 60);
+    function_result += std::to_string(sec);
+
+    function_result.erase(8);
     return function_result;
 }
-
 
 void save_time()
 {
@@ -58,19 +57,19 @@ void save_time()
         printf("clc. massage [error] : can't store last data time\n");
     }
 
-    return ;
+    return;
 }
 
 double load_time()
 {
     std::ifstream inFile(saved_time_file_path);
     double previous_time = 0;
-    if(inFile.is_open())
+    if (inFile.is_open())
     {
         inFile >> previous_time;
         inFile.close();
         return previous_time;
-        printf("clc. massage [alert] : last saved time loaded succesfully%f\n" ,previous_time);
+        printf("clc. massage [alert] : last saved time loaded succesfully%f\n", previous_time);
     }
     else
     {
@@ -79,41 +78,41 @@ double load_time()
     }
 }
 
-
 int main()
 {
     std::atexit(save_time);
-    
+
     bool order_time_stop = true;
 
     double last_time_time = load_time();
-    std::printf("clc. massage [alert]  : loaded time = %f\n",load_time());
+    std::printf("clc. massage [alert]  : loaded time = %f\n", load_time());
 
-    RGFW_window* RGFW_window_obj = RGFW_createWindow("clc.", 0, 0, 500, 300, RGFW_windowOpenGL | RGFW_windowNoBorder | RGFW_windowNoResize | RGFW_windowCenter);
+    RGFW_window *RGFW_window_obj = RGFW_createWindow(
+        "clc.", 0, 0, 500, 300, RGFW_windowOpenGL | RGFW_windowNoBorder | RGFW_windowNoResize | RGFW_windowCenter);
     RGFW_window_makeCurrentContext_OpenGL(RGFW_window_obj);
-    
+
     glyph_gl_set_opengl_version(3, 3);
     RGFW_window_show(RGFW_window_obj);
-    RGFW_window_setExitKey(RGFW_window_obj,RGFW_keyEscape);
-    glyph_renderer_t renderer = glyph_renderer_create(font_path, 135.0f,NULL, GLYPH_ENCODING_UTF8,NULL, 0);
+    RGFW_window_setExitKey(RGFW_window_obj, RGFW_keyEscape);
+    glyph_renderer_t renderer = glyph_renderer_create(font_path, 135.0f, NULL, GLYPH_ENCODING_UTF8, NULL, 0);
     glyph_renderer_set_projection(&renderer, 800, 600);
-    
+
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    std::chrono::time_point<std::chrono::high_resolution_clock> start_time; 
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
     saved_time += last_time_time;
-    while(RGFW_window_shouldClose(RGFW_window_obj) == false)
+    while (RGFW_window_shouldClose(RGFW_window_obj) == false)
     {
         RGFW_event RGFW_event_obj;
 
-        while(RGFW_window_checkEvent(RGFW_window_obj, &RGFW_event_obj))
+        while (RGFW_window_checkEvent(RGFW_window_obj, &RGFW_event_obj))
         {
-//          space
-            if(RGFW_event_obj.type == RGFW_keyPressed && RGFW_event_obj.button.value == RGFW_keySpace)    
+            // Space
+            if (RGFW_event_obj.type == RGFW_keyPressed && RGFW_event_obj.button.value == RGFW_keySpace)
             {
-                /// stop and start timer proc
-                if(order_time_stop == true)
+                /// Stop and start timer proc
+                if (order_time_stop == true)
                 {
                     order_time_stop = false;
                     start_time = std::chrono::high_resolution_clock::now();
@@ -124,49 +123,49 @@ int main()
                     saved_time += output_time;
                 }
             }
-//          r
-            if (RGFW_event_obj.type == RGFW_keyPressed &&  RGFW_event_obj.button.value == RGFW_keyR)
+            // R
+            if (RGFW_event_obj.type == RGFW_keyPressed && RGFW_event_obj.button.value == RGFW_keyR)
             {
                 saved_time = 0;
                 last_time_time = 0;
                 output_time = 0;
             }
-//          q
-            if (RGFW_event_obj.type == RGFW_keyPressed &&  RGFW_event_obj.button.value == RGFW_keyQ)
+            // Q
+            if (RGFW_event_obj.type == RGFW_keyPressed && RGFW_event_obj.button.value == RGFW_keyQ)
             {
                 save_time();
                 return 0;
             }
-
         }
 
-//      graphic interface    
+        // Graphic interface
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        if(order_time_stop)
+        if (order_time_stop)
         {
             rus_time = saved_time;
             t_str = t_str_fucn(rus_time);
-                               
-            glyph_renderer_draw_text(&renderer, t_str.c_str(),170.0f, 350.0f, 1.0f, 1.0f, 1.0f, 1.0f, GLYPH_EFFECT_NONE);
+
+            glyph_renderer_draw_text(&renderer, t_str.c_str(), 170.0f, 350.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+                                     GLYPH_EFFECT_NONE);
         }
         else if (!order_time_stop)
-        {    
+        {
             auto now_time = std::chrono::high_resolution_clock::now();
             output_time = std::chrono::duration<double>(now_time - start_time).count();
 
             rus_time = output_time + saved_time;
             t_str = t_str_fucn(rus_time);
-       
-            glyph_renderer_draw_text(&renderer, t_str.c_str(),170.0f, 350.0f, 1.0f, 1.0f, 1.0f, 1.0f, GLYPH_EFFECT_NONE);
+
+            glyph_renderer_draw_text(&renderer, t_str.c_str(), 170.0f, 350.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+                                     GLYPH_EFFECT_NONE);
         }
 
-        glyph_renderer_draw_text(&renderer,"clc.", 10, 100, 1.0f, 1.0f, 1.0f, 1.0f, GLYPH_EFFECT_NONE);
+        glyph_renderer_draw_text(&renderer, "clc.", 10, 100, 1.0f, 1.0f, 1.0f, 1.0f, GLYPH_EFFECT_NONE);
         RGFW_window_swapBuffers_OpenGL(RGFW_window_obj);
 
-        drawCircle(200,200,10, 10);
-
+        drawCircle(200, 200, 10, 10);
     }
     RGFW_window_close(RGFW_window_obj);
 
